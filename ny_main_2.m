dab36233e441f00f8fb0a395da9b0a2c2f12709c
jgrk <e.o.duvander@gmail.com>
2022-12-07 17:19:51 +0100

    börjat på b

--- ny_main_2.m
+++ ny_main_2.m
@@ -9,7 +9,7 @@ d=2.37;
 m=0.026;
 V0=13;
 h=1.85;
-grad=5;
+grad=4.09;
 g=9.82;
 Kx=0.001;
 Ky=0.01;
@@ -33,9 +33,8 @@ for iter = 1:maxiter
 
     trff_pnkt(1) = 0;
     
-    
-    
-    for t=0:dt:2
+    %rk4
+    for t=0:dt:.2
     
         x(end+1)=x(end)+u(end)*dt;
         y(end+1)=y(end)+v(end)*dt;
@@ -56,7 +55,7 @@ for iter = 1:maxiter
     
     end
     
-    t=0:dt:2+dt;
+    t=0:dt:.2+dt;
     
     %Binärsök
     if d < x(1)
@@ -76,6 +75,7 @@ for iter = 1:maxiter
         
         elseif d > x(mid)
             lo = mid + 1;
+
         else
             break
         end
@@ -127,6 +127,102 @@ plot(t,arrayfun(x_func,t),t,arrayfun(y_func,t))
 
 
 disp("träffpunkt från marken: " + svar )
+
+%%
+
+%b)
+
+clear; close all; clc;
+
+theta = 0:90;
+plot(theta, arrayfun(@f, theta))
+
+%sekantmetoden
+
+gr1 = 5; gr2 = 6; trunc = 1;
+
+while abs(trunc) > 10^-3
+
+    trunc = f(gr1) * (gr1 - gr2) / (f(gr1) - f(gr2));
+    r = gr1 - trunc;
+    gr2 = gr1;
+    gr1 = r;
+    
+
+end
+
+
+
+function trff=f(grad)
+
+%Givna konstanter och funktioner
+
+d=2.37;
+m=0.026;
+V0=13;
+h=1.85;
+g=9.82;
+Kx=0.001;
+Ky=0.01;
+be=1.83;
+
+udot=@(u,v)(-(Kx/m)*u*sqrt(u^2+v^2));
+vdot=@(u,v)(-g-(Ky/m)*v*sqrt(u^2+v^2));
+
+dt = 6.103515625000000e-06;
+u(1)=V0*cos((grad/360)*2*pi);
+v(1)=V0*sin((grad/360)*2*pi);
+x(1)=0; 
+y(1)=h; 
+
+for t=0:dt:.2
+    
+    x(end+1)=x(end)+u(end)*dt;
+    y(end+1)=y(end)+v(end)*dt;
+    
+    k1=udot(u(end),v(end));
+    l1=vdot(u(end),v(end));
+    k2=udot(u(end)+k1*dt/2, v(end)+l1*dt/2);
+    l2=vdot(u(end)+k1*dt/2, v(end)+l1*dt/2);
+    k3=udot(u(end)+k2*dt/2, v(end)+l2*dt/2);
+    l3=vdot(u(end)+k2*dt/2, v(end)+l2*dt/2);
+    k4=udot(u(end)+k3*dt, v(end)+l3*dt);
+    l4=vdot(u(end)+k3*dt, v(end)+l3*dt);
+
+    u(end+1)=u(end)+dt*(k1+2*k2+2*k3+k4)/6;
+    v(end+1)=v(end)+dt*(l1+2*l2+2*l3+l4)/6;
+
+end
+
+t=0:dt:.2+dt;
+
+%Binärsök
+if d < x(1)
+    idx=1;
+end
+
+if d > x(end)
+    idx=length(x);
+end
+
+hi = length(x);
+lo = 1;
+while lo <= hi
+    mid = fix( ( hi + lo ) * .5 );
+    if d < x(mid) 
+        hi = mid - 1;
+    
+    elseif d > x(mid)
+        lo = mid + 1;
+
+    else
+        break
+    end
+end
+
+trff = y(mid) - be;
+
+end
 
 
 
